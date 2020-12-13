82656c2dced6a2c8782622a2c8ccf0861ca8275c
caugustoarruda <caugustogarruda@gmail.com>
2020-12-13 14:08:47 -0300

    Enviando listagem de perguntas para o frontend

--- index.js
+++ index.js
@@ -26,7 +26,11 @@ app.use(bodyParser.json())
 
 // rotas
 app.get("/",(req, res) =>{
-    res.render("index")
+    Pergunta.findAll({raw:true}).then(perguntas => {
+        res.render("index", {
+            perguntas: perguntas
+        })
+    })
 })
 
 app.get("/perguntar", (req, res) =>{
@@ -41,7 +45,7 @@ app.post("/salvarpergunta", (req, res) =>{
         titulo: titulo,
         descricao: descricao
     }).then(() => {
-        res.redirect('/')
+        res.redirect("/")
     })
 })
 
